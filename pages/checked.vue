edaae36bb9375e941d7404a10fc205f0e853a7e9
sohep alslamat <sohep.alslamat91@gmail.com>
2020-04-06 21:49:35 +0300

    delsete meta tag twitter

--- pages/checked.vue
+++ pages/checked.vue
@@ -131,13 +131,9 @@ export default {
     return {
       title: ' تأكد من عدم اصابتك من فيروس كورونا | افحص نفسك',
       meta: [
-        { hid: 'og:title', name: 'og:title', content: 'تأكد من عدم اصابتك من فيروس كورونا - افحص نفسك' },
-        { hid: 'og:description', name: 'og:description', content: 'بروتوكل لتحديد حالات الاشتباه بفايروس كورونا من عدمه، إذ يعتمد على استخدام نظام النقاط' },
-        { hid: 'og:image', name: 'og:image', content: 'https://i.ibb.co/QKVHMv4/check.jpg' },
-        { hid: 'twitter:title', name: 'twitter:title', content: 'تأكد من عدم اصابتك من فيروس كورونا - افحص نفسك' },
-        { hid: 'twitter:description', name: 'twitter:description', content: 'بروتوكل لتحديد حالات الاشتباه بفايروس كورونا من عدمه، إذ يعتمد على استخدام نظام النقاط' },
-        { hid: 'twitter:image', name: 'twitter:image', content: 'https://i.ibb.co/QKVHMv4/check.jpg' }
-
+        { name: 'og:title', content: 'تأكد من عدم اصابتك من فيروس كورونا - افحص نفسك' },
+        { name: 'og:description', content: 'بروتوكل لتحديد حالات الاشتباه بفايروس كورونا من عدمه، إذ يعتمد على استخدام نظام النقاط' },
+        { name: 'og:image', content: 'https://i.ibb.co/QKVHMv4/check.jpg' }
       ]
     }
   }
